17deae4e9e79712f3abed0f0f2244c83ed7d0a69
ttnguyen2 <lil.thieninho@gmail.com>
2025-09-24 16:06:44 +0700

    updateee

--- src/services/socketClient.js
+++ src/services/socketClient.js
@@ -1,7 +1,7 @@
     let socket = null;
     let listeners = [];
 
-    export function initSocket(url = "ws://10.84.31.38:3000") {
+    export function initSocket(url = "wss://10.84.31.38:3000") {
     if (socket) return socket; // tránh tạo lại nhiều lần
     console.log("CẢNH BÁO! KHÔNG CÓ GÌ ĐÂU HEHE")
     socket = new WebSocket(url);
